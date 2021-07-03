a44916a6f7abb913ab037a99ff565e64c637e458
Mannan bansal <t-mbansal@microsoft.com>
2021-07-03 17:21:28 +0530

    Added mongoURI in env file

--- myApp.js
+++ myApp.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
+var mongoose = require('mongoose')
 
+
+mongoose.connect(process.env.MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
 
 let Person;
 
